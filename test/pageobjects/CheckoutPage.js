35dcb0092d72e5835d777cab8272075de147d65d
zeljkocvetic-lvx <zeljko.cvetic@leverx.com>
2025-11-11 14:33:55 +0100

    test found the products, it crashed on checkout

--- test/pageobjects/CheckoutPage.js
+++ test/pageobjects/CheckoutPage.js
@@ -4,13 +4,16 @@ import { attachScreenshot } from '../helpers/screenshotHelper.js';
 class CheckoutPage {
 
     constructor() {
-        this.CART_ITEM_SELECTOR = {
+        // Generic selector for any cart entry
+        this.CART_ENTRY_SELECTOR = {
             elementProperties: {
                 viewName: "sap.ui.demo.cart.view.Cart",
-                metadata: "sap.m.ObjectListItem"
+                metadata: "sap.m.ObjectListItem",
+                bindingContextPath: "/cartEntries/*"
             }
         };
 
+        // Checkout button
         this.CHECKOUT_BUTTON_SELECTOR = {
             elementProperties: {
                 viewName: "sap.ui.demo.cart.view.Cart",
@@ -18,21 +21,35 @@ class CheckoutPage {
                 text: [{ path: "i18n>checkout" }]
             }
         };
+
+        // Page title to ensure cart loaded
+        this.PAGE_TITLE_SELECTOR = {
+            elementProperties: {
+                viewName: "sap.ui.demo.cart.view.Cart",
+                metadata: "sap.m.Title",
+                id: "*page-title"
+            }
+        };
     }
 
-    async waitForPageLoaded() {
-        await ui5.userInteraction.waitForDisplayed(this.CART_ITEM_SELECTOR, 5000);
-        await attachScreenshot('Checkout Page Loaded');
+    // Wait until cart page is fully loaded
+    async waitForPageLoaded(timeout = 5000) {
+        await ui5.element.waitForAll(this.PAGE_TITLE_SELECTOR, timeout);
+        await attachScreenshot('Cart Page Loaded');
     }
 
+    // Get all displayed cart items (name, quantity, price)
     async getCartItems() {
-        const elements = await ui5.element.getDisplayed(this.CART_ITEM_SELECTOR);
-        const items = [];
+        const elements = await ui5.element.getAllDisplayed(this.CART_ENTRY_SELECTOR);
+        if (elements.length === 0) {
+            throw new Error('No products displayed in cart');
+        }
 
+        const items = [];
         for (let i = 0; i < elements.length; i++) {
-            const name = await ui5.element.getPropertyValue(this.CART_ITEM_SELECTOR, 'title', i);
-            const quantity = parseInt(await ui5.element.getPropertyValue(this.CART_ITEM_SELECTOR, 'number', i));
-            const priceText = await ui5.element.getPropertyValue(this.CART_ITEM_SELECTOR, 'number', i); // adjust if price is in different property
+            const name = await ui5.element.getPropertyValue(this.CART_ENTRY_SELECTOR, 'title', i);
+            const quantity = parseInt(await ui5.element.getPropertyValue(this.CART_ENTRY_SELECTOR, 'intro', i));
+            const priceText = await ui5.element.getPropertyValue(this.CART_ENTRY_SELECTOR, 'number', i);
             const price = parseFloat(priceText);
 
             items.push({ name, quantity, price });
@@ -41,10 +58,33 @@ class CheckoutPage {
         return items;
     }
 
+    // Get quantity for a specific product
+    async getQuantityForProduct(productName) {
+        const selector = {
+            elementProperties: {
+                viewName: "sap.ui.demo.cart.view.Cart",
+                metadata: "sap.m.ObjectListItem",
+                title: productName,
+                bindingContextPath: "/cartEntries/*"
+            }
+        };
+        const quantityText = await ui5.control.getProperty(selector, "intro");
+        return parseInt(quantityText);
+    }
+
+    // Get total number of items in cart
+    async getTotalCartItems() {
+        const items = await ui5.element.getAllDisplayed(this.CART_ENTRY_SELECTOR);
+        return items.length;
+    }
+
+    // Proceed to checkout
     async proceedToCheckout() {
+        await ui5.userInteraction.waitForDisplayed(this.CHECKOUT_BUTTON_SELECTOR, 5000);
         await ui5.userInteraction.click(this.CHECKOUT_BUTTON_SELECTOR);
         await attachScreenshot('Proceeded to Checkout');
     }
+
 }
 
 export default new CheckoutPage();
