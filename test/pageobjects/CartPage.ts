612f6da8c76e8fc3520939041d6533ebaa0ef0ae
zeljkocvetic-lvx <zeljko.cvetic@leverx.com>
2025-11-25 11:31:59 +0100

    Selectors in cart page made static

--- test/pageobjects/CartPage.ts
+++ test/pageobjects/CartPage.ts
@@ -1,50 +1,45 @@
 import { CartItem } from '../support/types.ts';
 import { BasePage } from './BasePage.ts';
+import { QmateSelector } from 'wdio-qmate-service/modules/ui5/types/ui5.types';
+
 
 class CartPage extends BasePage {
-    CART_ITEM_SELECTOR: any;
-    CART_BUTTON_SELECTOR: any;
-    CART_PAGE_TITLE_SELECTOR: any;
 
-    constructor() {
-        super();
+    static readonly CART_ITEM_SELECTOR: QmateSelector = {
+        elementProperties: {
+            viewName: "sap.ui.demo.cart.view.Cart",
+            metadata: "sap.m.ObjectListItem"
+        }
+    };
 
-        this.CART_ITEM_SELECTOR = {
-            elementProperties: {
-                viewName: "sap.ui.demo.cart.view.Cart",
-                metadata: "sap.m.ObjectListItem"
-            }
-        };
+    static readonly CART_BUTTON_SELECTOR: QmateSelector = {
+        elementProperties: {
+            viewName: "sap.ui.demo.cart.view.Home",
+            metadata: "sap.m.ToggleButton"
+        }
+    };
 
-        this.CART_BUTTON_SELECTOR = {
-            elementProperties: {
-                viewName: "sap.ui.demo.cart.view.Home",
-                metadata: "sap.m.ToggleButton"
-            }
-        };
-
-        this.CART_PAGE_TITLE_SELECTOR = {
-            elementProperties: {
-                viewName: "sap.ui.demo.cart.view.Cart",
-                metadata: "sap.m.Title",
-                id: "*page-title"
-            }
-        };
-    }
+    static readonly CART_PAGE_TITLE_SELECTOR: QmateSelector = {
+        elementProperties: {
+            viewName: "sap.ui.demo.cart.view.Cart",
+            metadata: "sap.m.Title",
+            id: "*page-title"
+        }
+    };
 
     async openCart(): Promise<void> {
-        await ui5.userInteraction.click(this.CART_BUTTON_SELECTOR);
+        await ui5.userInteraction.click(CartPage.CART_BUTTON_SELECTOR);
     }
 
     async getCartItems(): Promise<CartItem[]> {
-        const elements = await ui5.element.getAllDisplayed(this.CART_ITEM_SELECTOR);
+        const elements = await ui5.element.getAllDisplayed(CartPage.CART_ITEM_SELECTOR);
         const items: CartItem[] = [];
 
         for (let i = 0; i < elements.length; i++) {
             const product: CartItem = {
-                name: await ui5.element.getPropertyValue(this.CART_ITEM_SELECTOR, 'title', i),
-                quantity: parseInt(await ui5.element.getPropertyValue(this.CART_ITEM_SELECTOR, 'intro', i)),
-                price: parseFloat(await ui5.element.getPropertyValue(this.CART_ITEM_SELECTOR, 'number', i))
+                name: await ui5.element.getPropertyValue(CartPage.CART_ITEM_SELECTOR, 'title', i),
+                quantity: parseInt(await ui5.element.getPropertyValue(CartPage.CART_ITEM_SELECTOR, 'intro', i)),
+                price: parseFloat(await ui5.element.getPropertyValue(CartPage.CART_ITEM_SELECTOR, 'number', i))
             };
             items.push(product);
         }
@@ -53,7 +48,7 @@ class CartPage extends BasePage {
     }
 
     async waitForPageLoaded(): Promise<void> {
-        await ui5.element.getDisplayed(this.CART_PAGE_TITLE_SELECTOR);
+        await ui5.element.getDisplayed(CartPage.CART_PAGE_TITLE_SELECTOR);
     }
 }
 
